22566d51bd4c20ba3e8853cf99ee16f7b32d9f9f
HoanDV <57141929+Hoanxyz@users.noreply.github.com>
2023-10-23 23:00:16 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { ToggleComponent } from './toggle/toggle.component';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('toggleComp', {static: true}) toggleComp!: ToggleComponent;
+  // @ViewChild(ToggleComponent) toggleComp!: ToggleComponent;
+  @ViewChildren(ToggleComponent) toggleComps!: QueryList<ToggleComponent>;
+  @ViewChild('toggleBtn', {static: true}) toggleBtn!: ElementRef<HTMLButtonElement>;
+  @ViewChild('nameInput', {static: true}) nameInput!: ElementRef<HTMLInputElement>;
   title = 'train-angular';
   currentProgress = 70;
   name = "Angular";
   isChecked = true;
+  showLast = true;
+  question = {
+    question1: true,
+    question2: false
+  }
+
+  ngOnInit() {
+    console.log('On Init: ', this.toggleComp, this.toggleBtn);
+    this.nameInput.nativeElement.focus();
+    this.toggleBtn.nativeElement.addEventListener('click', () => {this.toggleComp.toggle()});
+  }
+
+  ngAfterViewInit() {
+    console.log(this.toggleComp);
+    setTimeout(() => {this.toggleComp.toggle()}, 1000);
+    this.toggleComps.changes.subscribe(console.log);
+  }
 }
--- src/app/toggle/toggle.component.ts
+++ src/app/toggle/toggle.component.ts
@@ -7,5 +7,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ToggleComponent {
   @Input() checked!: boolean;
-  @Output() checkChange = new EventEmitter<boolean>()
+  @Output() checkedChange = new EventEmitter<boolean>();
+  @Input() header!: string;
+  @Input() question!: string;
+  @Input() content!: string;
+  // @Input() canSkip!: boolean;
+  // @Output() skip = new EventEmitter<boolean>();
+
+  toggle() {
+    this.checkedChange.emit(!this.checked);
+  }
 }
